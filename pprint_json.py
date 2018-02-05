cc6ab468be85cbc5c50dd43cea81501b389c5d1c
kaptsov <kaptsovaa@gmail.com>
2018-02-06 08:33:12 +0900

    парсинг строки вынесен в отдельную функцию
    поправлены кавычки и метод чтения файла

--- pprint_json.py
+++ pprint_json.py
@@ -2,22 +2,21 @@ import json
 import argparse
 
 def load_data(filepath):
-    json_file = open(filepath, "r")
-    filedata = json_file.read()
-    json_file.close()
+    with open(filepath) as json_file:
+        filedata = json_file.read()
     return json.loads(filedata)
 
 def pretty_print_json(json_data):
     print(json.dumps(json_data, sort_keys=True, indent=2, ensure_ascii = False))
 
-
-if __name__ == '__main__':
+def commandline_link_parser():
     parser = argparse.ArgumentParser(description='Get the link to json file.')
     parser.add_argument('filepath', type=str)
-    args = parser.parse_args()
-    filepath = args.filepath
+    return parser.parse_args().filepath
 
-    json_data = load_data(filepath)
+
+if __name__ == '__main__':
+    json_data = load_data(commandline_link_parser())
     pretty_print_json(json_data)
 
 
