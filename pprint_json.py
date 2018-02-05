99e3b3ec69b2c27994d9441a58b43bab92051e54
kaptsov <kaptsovaa@gmail.com>
2018-02-06 08:48:52 +0900

    Изменения по pep8

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,17 @@
 import json
 import argparse
 
+
 def load_data(filepath):
     with open(filepath) as json_file:
         filedata = json_file.read()
     return json.loads(filedata)
 
+
 def pretty_print_json(json_data):
-    print(json.dumps(json_data, sort_keys=True, indent=2, ensure_ascii = False))
+    print(json.dumps(json_data, sort_keys=True,
+          indent=2, ensure_ascii=False))
+
 
 def commandline_link_parser():
     parser = argparse.ArgumentParser(description='Get the link to json file.')
@@ -18,5 +22,3 @@ def commandline_link_parser():
 if __name__ == '__main__':
     json_data = load_data(commandline_link_parser())
     pretty_print_json(json_data)
-
-
