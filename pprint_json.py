ab021e7cb9eefee9cf78a60d58a8fd904c452914
kaptsov <kaptsovaa@gmail.com>
2018-02-05 15:21:10 +0900

    Добавлена возможность загружать строку из консоли

--- pprint_json.py
+++ pprint_json.py
@@ -1,18 +1,22 @@
 import json
-
+import argparse
 
 def load_data(filepath):
     f = open(filepath, "r")
     filedata = f.read()
     f.close()
-    return filedata
+    return json.loads(filedata)
 
 def pretty_print_json(data):
-    print(json.dumps(json.loads(data), sort_keys=True, indent=2, ensure_ascii = False))
+    print(json.dumps(data, sort_keys=True, indent=2, ensure_ascii = False))
 
 
 if __name__ == '__main__':
-    data = load_data(input('Input a link: '))
+    parser = argparse.ArgumentParser(description='Get the link to json file.')
+    parser.add_argument('filepath', type=str)
+    args = parser.parse_args()
+    filepath = args.filepath
+    data = load_data(filepath)
     pretty_print_json(data)
 
 
